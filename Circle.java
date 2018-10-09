46c6c4e89fa2b6debde6efaae0780f495788e770
Áulus <aulus97@gmail.com>
2018-10-09 11:46:44 -0300

    Before "Desafio 3"

--- Circle.java
+++ Circle.java
@@ -1,24 +1,36 @@
 
-public class Circle {          
-	// salvar como "Circle.java"
+public class Circle extends Point {          
 	// variáveis de instancia privadas, isto é, não acessíveis de fora desta classe.
 	private double radius;
 	private String color;
+	private double length;
+	
 	// primeiro construtor o qual atribui valores iniciais a ambos: radius e color .
 	public Circle() {
 		radius = 1.0;
 		color = "red";
+		length = 2*radius*Math.PI;
 	}
-	// Segundo construtor que inicia radius com o parâmetro recebido, e matem color com 
-	//o valor padrão definido.
-	public Circle(double r) {
-		this();   
-	// cria o objeto com o primeiro construtor:.Circle()color = "red";
+	
+	// Segundo construtor que inicia radius com o parâmetro recebido, e matem color com o valor padrão definido.
+	public Circle(double radius) {
+		this.radius = radius;   
+		color = "red"; // cria o objeto com o primeiro construtor:.Circle()
 	}
-	public Circle(double r, String c){
-		this();
-		radius = r;
-		color = c;
+	public Circle(double radius, String color){
+		this.radius = radius;
+		this.color = color;
+	}
+	public String toString() {   
+		return "Circulo: raio = " + radius + " cor = " + color;   
+	}
+	// metodo Set para a variável de instancia radius
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	// metdodo Set para a variável de instancia  color
+	public void setColor(String color) {
+		this.color = color;
 	}
 	// Metodo de acesso para obter o valor armazenado em radius
 	public double getRadius() {
@@ -28,15 +40,12 @@ public class Circle {
 	public double getArea() {
 		return radius*radius*Math.PI; // PI é a constante. Math é a classe onde PI é definido
 	}
+	public double getLength() {
+		return length;
+	}
 	public String getColor() {
 		return color;
 	}
-	// metodo Set para a variável de instancia radius
-	public void setRadius(double radius) {
-		
-	}
-	// metdodo Set para a variável de instancia  color
-	public void setColor(String color) {
-	}
+
 }
 //nao compilou por falta de uma main
--- TestCircle.java
+++ TestCircle.java
@@ -2,33 +2,53 @@
 public class TestCircle {       
 // salve como "TestCircle.java"
 	public static void main(String[] args) {
-		//  Declara  c1	como  variável  habilitada  a  armazenar  uma  referencia  para  objeto  da  classe	Circle.
+		//  Declara  c1	como  variÃ¡vel  habilitada  a  armazenar  uma  referencia  para  objeto  da  classe	Circle.
 		Circle c1; 
-		// atribui a c1 .a referencia retornada pelo construtor padrão Circle () 
+		
+		// atribui a c1 .a referencia retornada pelo construtor padrÃ£o Circle () 
 		c1 	= new Circle();
-		// Para invocar os metodos classe Circle para	operar sobre a instância c1,
-		// usa-se o operador ponto (“.”). 
-		//Em outras palavras: usa-se o ponto para enviar uma mensagem ao objeto c1 para que 
-		// ele execute um de seus métodos.
+		
+		/* Para invocar os metodos classe Circle para	operar sobre a instÃ¢ncia c1,usa-se o operador ponto (â€œ.â€). 
+		Em outras palavras: usa-se o ponto para enviar uma mensagem ao objeto c1 para que ele execute um de seus mÃ©todos.
+		*/
+		
 		System.out.println("O circulo tem o raio de "+ c1.getRadius() + " e area de "+ c1.getArea());
-		// Declara e aloca uma segunda instancia da classe Circle chamada c2 
-		// 	com o valor do radius igual a 2.0 	e color	com valor padrão.
-		Circle c2 = new Circle(2.0);
-		// Para invocar os metodos 	a oerar sobre a instância c2, usa-	se o operador ponto (“.”)
-		System.out.println("O circulo tem raio de "+ c2.getRadius() + " e area de "+ c2.getArea());
-		// Declara e aloca uma segunda instancia da classe Circle chamada c2 
-		// 	com o valor do radius igual a 2.0 	e color	com valor azul.
-		Circle c3 = new Circle(2.0,"azul");
-		// Para invocar os metodos 	a oerar sobre a instância c2, usa-	se o operador ponto (“.”)
+		
+		/* Declara e aloca uma segunda instancia da classe Circle chamada c2 
+			com o valor do radius igual a 2.0 	e color	com valor padrÃ£o.
+		*/
+		
+		//Circle c2 = new Circle(2.0);
+		
+		// Para invocar os metodos 	a oerar sobre a instÃ¢ncia c2, usa-	se o operador ponto (â€œ.â€)
+		
+		//System.out.println("O circulo tem raio de "+ c2.getRadius() + " e area de "+ c2.getArea());
+		
+		 Circle c2 = new Circle(1.2); 
+		 
+	     System.out.println(c2.toString());  // chamada explicita 
+	     
+	     System.out.println(c2);// println() chama toString() implicitamente 
+	     
+	  // no exemplo abaixo o operador '+' invoca c2.toString() implicitamente. 
+	     System.out.println("Aqui o operador '+' também invoca toString(): " + c2); 
+	     
+		Circle c3 = new Circle();
+		c3.setRadius(5.0);
+		c3.setColor("green");
+		
+		// Para invocar os metodos 	a oerar sobre a instÃ¢ncia c3, usa-	se o operador ponto (â€œ.â€)
 		System.out.println("O circulo tem raio de "+ c3.getRadius() + " e area de "+ c3.getArea());
 		
 		//imprimindo o getColor
 		System.out.println("O circulo tem raio de "+ c3.getRadius() + ", area de "+ c3.getArea() + " e cor " + c3.getColor());
 		
 		//c1.radius=5.0;
+		
+		System.out.println(c1.toString()); // chamada explicita
 
 	}
-	// fim do método  main()
+	// fim do mÃ©todo  main()
 }
 // fim da classe TestCircle
 
@@ -58,8 +78,25 @@ The field Circle.radius is not visible
 at TestCircle.main(TestCircle.java:28)
 */
 
+/* Item 9a:
 
+O circulo tem o raio de 1.0 e area de 3.141592653589793
+O circulo tem raio de 2.0 e area de 12.566370614359172
+O circulo tem raio de 5.0 e area de 78.53981633974483
+O circulo tem raio de 5.0, area de 78.53981633974483 e cor green
+Circulo: raio = 1.0 cor = red
+ */
 
-
+/* Item 9B
+ REalmente invoca o toString >> Fantastico
+ O circulo tem o raio de 1.0 e area de 3.141592653589793
+Circulo: raio = 1.2 cor = red   <<<
+Circulo: raio = 1.2 cor = red   <<<
+Aqui o operador '+' também invoca toString(): Circulo: raio = 1.2 cor = red   <<<
+O circulo tem raio de 5.0 e area de 78.53981633974483
+O circulo tem raio de 5.0, area de 78.53981633974483 e cor green
+Circulo: raio = 1.0 cor = red
+ 
+ */
 
 
